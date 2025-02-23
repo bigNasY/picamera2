854a28e6ae258c9bebc035df23c878e7c8f80533
Bobby Smith <bobby.j.smith@icloud.com>
2025-02-23 14:16:25 -0500

    gui expansion

--- QTapp.py
+++ QTapp.py
@@ -1,29 +1,33 @@
 from picamera2 import Picamera2, Preview
 from picamera2.encoders import H264Encoder
 from libcamera import Transform, controls
-from PyQt5.QtWidgets import QApplication, QPushButton, QVBoxLayout, QWidget
+from PyQt5 import Qt
+from PyQt5.QtWidgets import *
 from picamera2.previews.qt import QGlPicamera2
 import cv2
+import time
 
-
-
-exposure_time = 100
+target = 0
+cur_task = ''
+exposure_time = 50000
 scale = 1.05
 picam2 = Picamera2()
+frame_rate = 50
+lens_pos = 32
 mode = picam2.sensor_modes[0]
-config = picam2.create_preview_configuration(main={"size" : (1280, 720)}, sensor={'output_size' : mode['size'], 'bit_depth' : mode['bit_depth']}, transform=Transform(vflip=True))
+print(mode)
+config = picam2.create_preview_configuration(main={"size" : (1280, 720)}, sensor={'output_size' : mode['size'], 'bit_depth' : mode['bit_depth']}, transform=Transform(vflip=False))
 picam2.align_configuration(config)
 picam2.configure(config)
-picam2.set_controls({"FrameRate" : 50})
-
-
-
+picam2.set_controls({"FrameRate" : frame_rate, 'ExposureTime' : exposure_time, 'LensPosition' : 32})
 
 
 def on_button1_clicked():
 	button1.setEnabled(False)
 	global scale
 	scale=0.95
+	global cur_task 
+	cur_task = 'zoom'
 	picam2.capture_metadata(signal_function=qpicamera2.signal_done)
 	
 
@@ -32,57 +36,195 @@ def on_button2_clicked():
 	button2.setEnabled(False)
 	global scale
 	scale = 1.05
+	global cur_task 
+	cur_task = 'zoom'
 	picam2.capture_metadata(signal_function=qpicamera2.signal_done)
 	
 
-	
-	
-	
-	
 
+def on_button3_clicked():
+	button3.setEnabled(False)
+	picam2.stop()
+	mode = picam2.sensor_modes[2]
+	print(mode)
+	config = picam2.create_preview_configuration(main={"size" : (1280, 720)}, sensor={'output_size' : mode['size'], 'bit_depth' : mode['bit_depth']}, transform=Transform(vflip=False))
+	picam2.align_configuration(config)
+	picam2.configure(config)
+	picam2.set_controls({"FrameRate" : frame_rate, 'ExposureTime' : exposure_time, "LensPosition" : lens_pos})
+	picam2.start()
+	button4.setEnabled(True)
+	button5.setEnabled(True)
+	
+	
+def on_button4_clicked():
+	button4.setEnabled(False)
+	picam2.stop()
+	mode = picam2.sensor_modes[0]
+	config = picam2.create_preview_configuration(main={"size" : (1280, 720)}, sensor={'output_size' : mode['size'], 'bit_depth' : mode['bit_depth']}, transform=Transform(vflip=False))
+	picam2.align_configuration(config)
+	picam2.configure(config)
+	picam2.set_controls({"FrameRate" : frame_rate, 'ExposureTime' : exposure_time, "LensPosition" : lens_pos})
+	picam2.start()
+	button3.setEnabled(True)
+	button5.setEnabled(True)
+
+	
+def on_button5_clicked():
+	button5.setEnabled(False)
+	picam2.stop()
+	mode = picam2.sensor_modes[1]
+	print(mode)
+	config = picam2.create_preview_configuration(main={"size" : (1280, 720)}, sensor={'output_size' : mode['size'], 'bit_depth' : mode['bit_depth']}, transform=Transform(vflip=False))
+	picam2.align_configuration(config)
+	picam2.configure(config)
+	picam2.set_controls({"FrameRate" : frame_rate, 'ExposureTime' : exposure_time, "LensPosition" : lens_pos})
+	picam2.start()
+	button3.setEnabled(True)
+	button4.setEnabled(True)
+	
+	
+def on_button6_clicked():
+	global cur_task
+	cur_task = 'fr'
+	t_end = time.time() + 5
+	count = 0
+	while time.time() < t_end:
+		picam2.capture_array("main", signal_function=qpicamera2.signal_done)
+		count += 1
+	
+	
+	fps = count/5
+	print(count)
+	print(f'FPS: {fps}')
+	
 
 
 def zoom_done(job):
+	global cur_task
 	result = picam2.wait(job)
-	full_res = picam2.camera_properties['PixelArraySize']
-	size = result['ScalerCrop'][2:]
-	newSize = [int(s * scale) for s in size]
-	offset = [(r - s) // 2 for r, s in zip(full_res, newSize)]
-	picam2.set_controls({'ScalerCrop' : offset+newSize})
-	button1.setEnabled(True)
-	button2.setEnabled(True)
+	if cur_task == 'zoom':
+		full_res = picam2.camera_properties['PixelArraySize']
+		size = result['ScalerCrop'][2:]
+		newSize = [int(s * scale) for s in size]
+		offset = [(r - s) // 2 for r, s in zip(full_res, newSize)]
+		picam2.set_controls({'ScalerCrop' : offset+newSize})
+		button1.setEnabled(True)
+		button2.setEnabled(True)
 	
 
+def value_changed(i):
+	global exposure_time
+	exposure_time = i*1000
+	picam2.set_controls({'ExposureTime' : i*1000})
 
 
+def text_changed(val):
+	try:
+		if int(val) >= 1 and int(val) <= 32: 
+			global lens_pos
+			lens_pos = int(val)
+			picam2.set_controls({'LensPosition' : int(val)})
+	except:
+		pass
 
 
-
+def frame_text_changed(val):
+	try:
+		f = int(val)
+		if(f > 0):
+			global frame_rate
+			frame_rate = f
+			picam2.set_controls({'FrameRate' : f})	
+	except:
+		pass
+		
 
 app = QApplication([])
-qpicamera2 = QGlPicamera2(picam2, width=800, height=600, keep_ar=False)
+qpicamera2 = QGlPicamera2(picam2, width=1280, height=720, keep_ar=False)
 
-button1 = QPushButton("Click to zoom")
+button1 = QPushButton("Click to zoom in")
 window = QWidget()
 qpicamera2.done_signal.connect(zoom_done)
 button1.clicked.connect(on_button1_clicked)
 
 
 button2 = QPushButton("Click to zoom out")
-
 button2.clicked.connect(on_button2_clicked)
 
+button3 = QPushButton("2592p")
+button3.clicked.connect(on_button3_clicked)
+
+button4 = QPushButton("864p")
+button4.clicked.connect(on_button4_clicked)
+button4.setEnabled(False)
+
+button5 = QPushButton("1296p")
+button5.clicked.connect(on_button5_clicked)
+
+button6 = QPushButton("Test Frame Rate")
+button6.clicked.connect(on_button6_clicked)
+
+expTimeSlider = QSlider()
+expTimeSlider.setMinimum(1)
+expTimeSlider.setMaximum(100)
+expTimeSlider.setSingleStep(1)
+expTimeSlider.valueChanged.connect(value_changed)
+expTimeSlider.setValue(50)
+
+
+label = QLabel()
+label.setText('Exposure Time')
+
+
+focalDistEdit = QLineEdit()
+focalDistEdit.setMaxLength(2)
+focalDistEdit.setPlaceholderText('Enter focal distance (1 to 32 cm)')
+focalDistEdit.textEdited.connect(text_changed)
+
+frameEdit = QLineEdit()
+frameEdit.setMaxLength(3)
+frameEdit.setPlaceholderText('Enter Frame Rate')
+frameEdit.textEdited.connect(frame_text_changed)
+
+label2 = QLabel()
+label2.setText('Frame Rate Test')
+
+
+
+layout = QHBoxLayout()
+
+
+layout_v3 = QHBoxLayout()
+layout_v3.addWidget(button4)
+layout_v3.addWidget(button5)
+layout_v3.addWidget(button3)
+
+layout_v4 = QHBoxLayout()
+layout_v4.addWidget(frameEdit)
+layout_v4.addWidget(button6)
 
 layout_v = QVBoxLayout()
+layout_v.setSpacing(10)
 layout_v.addWidget(qpicamera2)
 layout_v.addWidget(button1)
 layout_v.addWidget(button2)
+layout_v.addLayout(layout_v3)
+layout_v.addWidget(focalDistEdit)
+layout_v.addLayout(layout_v4)
 
+
+layout_v2 = QVBoxLayout()
+layout_v2.addWidget(label)
+layout_v2.addWidget(expTimeSlider)
+
+
+layout.addLayout(layout_v)
+layout.addLayout(layout_v2)
 
 
 qpicamera2.setWindowTitle("sigma")
-window.setLayout(layout_v)
-window.resize(640, 480)
+window.setLayout(layout)
+window.resize(800, 600)
 picam2.start()
 window.show()
 app.exec()
--- fov.py
+++ fov.py
@@ -3,20 +3,20 @@ from picamera2.encoders import H264Encoder
 from libcamera import Transform
 import cv2
 import time
-
+import math
 
 def main():
 	picam2 = Picamera2()
-	mode = picam2.sensor_modes[0]
+	mode = picam2.sensor_modes[2]
 	config = picam2.create_preview_configuration(main={"size" : (1280, 720), "format" : 'RGB888'}, sensor={'output_size' : mode['size'], 'bit_depth' : mode['bit_depth']}, transform=Transform(vflip=True))
 	picam2.align_configuration(config)
 	picam2.configure(config)
-	picam2.set_controls({"FrameRate" : 50})
+	picam2.set_controls({"FrameRate" : 50, "LensPosition" : 32})
 	picam2.start()
-	#cv2.imwrite('full.jpg', picam2.capture_array())
-	#print(picam2.camera_properties['PixelArraySize'])
-	#print(picam2.capture_metadata()['ScalerCrop'])
+
 	
+	
+	show_img(picam2)
 	change_fov(picam2, name="zoom.jpg", scale=0.6)
 	#half_fov(picam2)
 	picam2.stop()
@@ -26,16 +26,17 @@ def change_fov(picam2, name='test.jpg', scale=1):
 	#this changes fov only in a zoom/pan manner.
 	#It is possible to select portions of the image (e.g. just the top half), but that image is then scaled to a wonky aspect ratio.
 	full_res = picam2.camera_properties['PixelArraySize']
-	size = picam2.capture_metadata()['ScalerCrop'][2:]
 	
+	size = picam2.capture_metadata()['ScalerCrop'][2:]
+	print(f'new size should be: {math.floor(size[0]*0.6)} x {math.floor(size[1]*0.6)}')
 	newSize = [int(s * scale) for s in size]
 	offset = [(r - s) // 2 for r, s in zip(full_res, newSize)]
 	picam2.set_controls({"ScalerCrop": offset + newSize})
 	time.sleep(2)
-	picam2.capture_metadata()
-	img = picam2.capture_array()
-	cv2.imwrite(name,img)
-	#show_img(img)
+	#picam2.capture_metadata()
+	#img = picam2.capture_array()
+	#cv2.imwrite(name,img)
+	show_img(picam2)
 	
 	
 def half_fov(picam2):
@@ -47,15 +48,22 @@ def half_fov(picam2):
 	offset = (size[0], size[1] // 4)
 	picam2.set_controls({"ScalerCrop": offset + newSize})
 	time.sleep(2)
-	picam2.capture_metadata()
-	img = picam2.capture_array()
-	cv2.imwrite('half.jpg', img)
-	#show_img(img)
+	#picam2.capture_metadata()
+	#img = picam2.capture_array()
+	#cv2.imwrite('half.jpg', img)
+	show_img(picam2)
 	
 
 
-def show_img(img):
+def show_img(picam2):
+	full_res = picam2.camera_properties['PixelArraySize']
+	print(f'full res: {full_res} full aspect ratio: {round(full_res[0] / full_res[1], 2)}')
+	res = picam2.camera_properties['PixelArrayActiveAreas'][0]
+	size = picam2.capture_metadata()['ScalerCrop']
+	
 	while True:
+		img = picam2.capture_array()
+		img = cv2.putText(img, f'res: {res[2]} x {res[3]} size: {size[2]} x {size[3]} aspect ratio: {round(size[2] / size[3], 2)}', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
 		cv2.imshow("image", img)
 		if cv2.waitKey(1)==ord('q'):
 			cv2.destroyAllWindows()
